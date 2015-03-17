f36061b00aed43e548e3dd78eb7b36fe560825a3
Davide Gullo <jazzo72@gmail.com>
2015-03-17 14:48:01 +0100

    Removed useless code

--- inc/js/common.js
+++ inc/js/common.js
@@ -133,18 +133,6 @@ jQuery("html").niceScroll({
     cursorborder: 'none'
 });
 
-
- /* ==============================================
-lightbox
-=============================================== */
-    
-    jQuery('.open-popup-link').magnificPopup({
-        type: 'inline',
-        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-    });
-
-
-
  /* ==============================================
 Setting slider height on load
 =============================================== */
